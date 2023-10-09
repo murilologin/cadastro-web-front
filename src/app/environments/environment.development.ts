7ebf0b343ff1c6fbe0ea7a6ee64da5f4e36b2bd2
Jorge Murilo <murilo.login@gmail.com>
2023-10-09 19:52:24 -0300

    first commit

---
+++ src/app/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    //baseUrl: 'http://localhost:8082/',
+    //baseApiUrl: 'http://localhost:8082/api/'
+    baseUrl: 'https://jorge3093.c34.integrator.host/',
+    baseApiUrl: 'https://jorge3093.c34.integrator.host/cadastroweb/api/'
+};
---
+++ src/app/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: true,
+    //baseUrl: 'http://ec2-3-94-165-5.compute-1.amazonaws.com:8082/',
+    //baseApiUrl: 'http://ec2-3-94-165-5.compute-1.amazonaws.com:8082/api/'
+    //baseUrl: 'http://localhost:8082/',
+    //baseApiUrl: 'http://localhost:8082/api/'
+    baseUrl: 'https://jorge3093.c34.integrator.host/',
+    baseApiUrl: 'https://jorge3093.c34.integrator.host/api/'    
+};
